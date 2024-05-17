ad4e03536c373f734cc7fbb1136d3bdd7d2437c8
Jikkesh <jikkekumar98@gmail.com>
2024-05-17 15:06:49 +0530

    Update assembly info files

--- server/server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+222f0a0e6b877a41d723633e759d7b8de6cd662b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e3db9c015d7846afe9e6fce571fa392d72e9a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
